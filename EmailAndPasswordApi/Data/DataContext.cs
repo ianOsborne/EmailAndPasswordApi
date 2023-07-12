6b175dd5d3229809e919542b71bf3c86d6443c83
Oz <ian.cosborne@yahoo.com>
2023-07-12 15:12:41 -0400

    Configured Entity Framework Core initial setup

---
+++ EmailAndPasswordApi/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using EmailAndPasswordApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailAndPasswordApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //may change connection string to live in either app settings or environment variables, but for now, it can be here
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=userdb;Trusted_Connection=true");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- EmailAndPasswordApi/Program.cs
+++ EmailAndPasswordApi/Program.cs
@@ -1,9 +1,11 @@
+using EmailAndPasswordApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<DataContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
